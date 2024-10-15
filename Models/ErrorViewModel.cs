c491962ec868f5ad7dc7c3689060b5593e8ee770
yufei <tyfei0000@gmail.com>
2024-10-15 17:00:52 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HrInternWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
